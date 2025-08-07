41c66ba86f9f4e6370d1bab1507ab28fdca36956
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-08-07 20:26:17 +0530

    Updated app urls

---
+++ server/auth/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+app_name = "auth"
+
+urlpatterns = [
+    path(
+        "basic-auth/<str:username>/<str:password>/",
+        views.BasicAuthView.as_view(),
+        name="basic-auth",
+    ),
+    path("bearer/", views.BearerAuthView.as_view(), name="bearer"),
+    path(
+        "hidden-basic-auth/<str:username>/<str:password>/",
+        views.HiddenBasicAuthView.as_view(),
+        name="hidden-basic-auth",
+    ),
+    path(
+        "digest-auth/<str:qop>/<str:username>/<str:password>/",
+        views.DigestAuthView.as_view(),
+        name="digest-auth",
+    ),
+    path("validate-token/", views.TokenValidationView.as_view(), name="validate-token"),
+]
--- server/server/urls.py
+++ server/server/urls.py
@@ -32,11 +32,16 @@ schema_view = swagger_get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("api/", include("api.urls"), name="api"),
+    path("auth/", include("auth.urls"), name="auth"),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
