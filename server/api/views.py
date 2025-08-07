8ec5f0a8fb3db835614953636e99b6623d8ab23c
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-08-07 20:19:21 +0530

    Updated API view statuscodes

--- server/api/views.py
+++ server/api/views.py
@@ -2,15 +2,16 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
 @api_view(["GET"])
 def home(request):
-    return Response({"message": "Welcome to the API!"}, status=200)
+    return Response({"message": "Welcome to the API!"}, status=status.HTTP_200_OK)
 
 
 @api_view(["GET"])
 def health_check(request):
-    return Response({"status": "healthy"}, status=200)
+    return Response({"status": "healthy"}, status=status.HTTP_200_OK)
